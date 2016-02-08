0c6b2eafca2b995e11f24335061f7ea2b59cbacf
Franco Eusébio Garcia <francoegarcia@outlook.com>
2016-02-08 10:20:17 -0200

    Added missing dot and replaced spaces with tabs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil . ((indent-tabs-mode . t)
-         (tab-width . 4)
-         (fill-column . 80)))
+		 (tab-width . 4)
+		 (fill-column . 80)))
  (c++-mode . ((c-file-style . "k&r")
-              (c-basic-offset 4))))
+			  (c-basic-offset . 4))))
